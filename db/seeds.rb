76d862d8b8410123c39bc76e8c159d515a539912
Fabien James <fabts4@gmail.com>
2023-02-27 21:27:42 +0100

    seeds with photo + flashes + logout sur navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
 
-# User.create!(email: "blabla@gmail.com", password: "123456")
-Offer.create!(celebrity_name: "Sarko l'escroc", price: 100, category: "politicien", content: "Tour de magie, disparitiond de malette de billet", user_id: 2)
-Offer.create!(celebrity_name: "Youssef Polnareff", price: 1000, category: "chanteur", content: "Michel Polnareff des 1000 et 1 nuits", user_id: 2)
-Offer.create!(celebrity_name: "Jean Alidé", price: 10000, category: "chanteur", content: "Le Johnny Halliday français", user_id: 2)
+Offer.destroy_all if Rails.env.development?
+User.destroy_all if Rails.env.development?
+
+sarko_photo = URI.open("https://www.masculin.com/wp-content/uploads/sites/2/article/9289/sosie-de-merde-sarko-2.jpg")
+polna_photo = URI.open("http://loznareff.fr/images/loznareff-sosie-michel-polnareff-vac.jpg")
+alide_photo = URI.open("https://www.francebleu.fr/s3/cruiser-production/2018/12/a8908f45-6e96-40ef-ba27-fb7148612c36/1200x680_870x489_fullsizerender_29.jpg")
+
+user = User.create!(email: "sosie@gmail.com", password: "123456")
+
+sarko = Offer.new(celebrity_name: "Sarko l'escroc", price: 100, category: "Politician", content: "Tour de magie, disparition de malette de billet", user_id: user.id)
+sarko.photo.attach(io: sarko_photo, filename: "sarko.jpg", content_type: "image/jpg")
+sarko.save!
+
+polna = Offer.new(celebrity_name: "Youssef Polnareff", price: 1000, category: "Singer", content: "Michel Polnareff des 1000 et 1 nuits", user_id: user.id)
+polna.photo.attach(io: polna_photo, filename: "sarko.jpg", content_type: "image/jpg")
+polna.save!
+
+alide = Offer.new(celebrity_name: "Jean Alidé", price: 10000, category: "Singer", content: "Le Johnny Halliday français", user_id: user.id)
+alide.photo.attach(io: alide_photo, filename: "sarko.jpg", content_type: "image/jpg")
+alide.save
