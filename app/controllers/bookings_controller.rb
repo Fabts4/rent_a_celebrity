cd3709bf7eaad4fb282d36e4658b7462abc675b4
Edouard de Montchenu edouarddemontchenu@gmail.com <edouarddemontchenu@gmail.com>
2023-02-28 12:46:10 +0100

    adding booking feature

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,32 @@
+class BookingsController < ApplicationController
+  before_action :set_offer, only: %i[new create]
+
+  def new
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.offer = @offer
+    if @booking.save
+      redirect_to offer_path(@offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_offer
+    @offer = Offer.find(params[:offer_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:event_date, :comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "offers#index"
 
-  resources :offers, only: [:new, :create, :show, :index]
+  resources :offers, only: [:new, :create, :show, :index] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
