1821cd6d7bad144076a360905b285a2778616ec7
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <berdahal12@protonmail.com>
2023-03-03 13:13:42 +0100

    arrangement js

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
     new Typed(this.element, {
       strings: ["Vivez avec vos célébrités préférées le temps d'une soirée inoubliable"],
       typeSpeed: 50,
-      loop: true
+      
     })
   }
 }
